2b7e949392c859424fa56cb9301277cf11575d1b
Md Abu Talib <talisman702k@gmail.com>
2026-02-05 12:21:40 +0530

    how to execute code in javascript

---
+++ exectution.js
@@ -0,0 +1,15 @@
+//how to exectute a code in javaScript 
+
+var a =10
+console.log(a)  
+
+
+function add(a){
+    return a+a
+}
+
+function stepexecution(){
+    console.log("we are under stepexecution function   ")
+}
+
+stepexecution()
