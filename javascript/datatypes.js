8059def53b2aa45657730d26a341b66ce1820817
Md Abu Talib <talisman702k@gmail.com>
2026-02-10 00:52:24 +0530

    dataypes in js in short

---
+++ javascript/datatypes.js
@@ -0,0 +1,42 @@
+let name="Mohd Abutalib";
+let age=20;
+let isStudent=true;     
+let hobbies=["coding","reading","traveling"];
+let address
+let isPass = null;
+let bigNumber = 12345678901234567890n;
+let uniqueId = Symbol("id");
+let person = {
+    name: "Mohd Abutalib",
+    age: 20,
+    isStudent: true
+};
+
+console.log(name);
+console.log(typeof name);
+
+console.log(age);
+console.log(typeof age);
+
+console.log(isStudent);
+console.log(typeof isStudent);  
+       
+console.log(hobbies);
+console.log(typeof hobbies);
+
+console.log(address);
+console.log(typeof address);
+
+console.log(isPass);
+console.log(typeof isPass);
+
+console.log(bigNumber);
+console.log(typeof bigNumber);
+
+console.log(uniqueId);
+console.log(typeof uniqueId);   
+
+console.log(person);
+console.log(typeof person);
+
+
