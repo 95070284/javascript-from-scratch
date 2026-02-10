f0bce07f756b32cdc9e8acf342d9c84586270c53
Md Abu Talib <talisman702k@gmail.com>
2026-02-10 10:12:56 +0530

    operator in js

---
+++ javascript/operator.js
@@ -0,0 +1,76 @@
+let a =4;
+let b =2; 
+
+console.log("Addition:", a + b);
+// Output: 6
+
+console.log("Subtraction:", a - b);
+//subtraction: 2
+
+console.log("Multiplication:", a * b);
+//Multiplication: 8
+
+console.log("Division:", a / b);
+//Division: 2
+
+console.log("Modulus:", a % b); 
+//Modulus: 0
+
+console.log("Exponentiation:", a ** b);
+//Exponentiation: 16
+
+
+console.log("Increment:", a++);
+//Increment: 4
+
+
+console.log("After Increment:", a);
+//After Increment: 5
+
+
+console.log("Decrement:", b--);
+//Decrement: 2;
+
+
+console.log("After Decrement:", b);
+//After Decrement: 1
+
+console.log("Assignment:", a += b);
+//Assignment: 6
+
+console.log("Comparison (==):", a == b);
+//Comparison (==): false
+console.log("Comparison (===):", a === b);
+//Comparison (===): false
+
+
+console.log("Comparison (!=):", a != b);
+//Comparison (!=): true
+
+console.log("Comparison (!==):", a !== b);
+//Comparison (!==): true
+
+console.log("Greater than:", a > b);    
+//Greater than: true
+
+console.log("Less than:", a < b);
+//Less than: false
+
+console.log("Greater than or equal to:", a >= b);
+//Greater than or equal to: true
+
+console.log("Less than or equal to:", a <= b);
+//Less than or equal to: false
+
+console.log("Logical AND:", (a > 0) && (b > 0));
+//Logical AND: true
+
+console.log("Logical OR:", (a > 0) || (b > 0));
+//Logical OR: true
+
+console.log("Logical NOT:", !(a > 0));  
+//Logical NOT: false
+
+console.log("Ternary Operator:", (a > b) ? "a is greater" : "b is greater");    
+//Ternary Operator: a is greater
+
