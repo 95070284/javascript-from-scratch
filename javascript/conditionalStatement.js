56ae0f7bd8d723f9293a22bdf6da87b36ec97e05
Md Abu Talib <talisman702k@gmail.com>
2026-02-10 23:57:26 +0530

    conditional statement learning phase

---
+++ javascript/conditionalStatement.js
@@ -0,0 +1,26 @@
+
+let a=10;
+let b=5;
+
+if( a>b===true){
+    console.log(" a is greater than b")
+}
+else 
+{
+    console.log(" b is greater than a ")
+}
+
+
+// if else ladder 
+
+let mode="dark";
+
+if ( mode=== "dark" ){ 
+    console.log(" black ")
+}
+else if ( mode === "light" ){
+    console.log(" white ")
+}   
+else {
+    console.log(" invalid mode ")
+}
