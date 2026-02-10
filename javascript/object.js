2be35fd7281e10b155748750d72f90fb2375d3d6
Md Abu Talib <talisman702k@gmail.com>
2026-02-10 10:02:47 +0530

    object in js

---
+++ javascript/object.js
@@ -0,0 +1,13 @@
+const product ={
+    name: "Parker Jotter Pen",
+    price: 10.99,
+    color: "blue",
+    star:4,
+    MRP :2799,
+    offer: "60% off",
+    finalPrice:(2799*60)/100,
+    inStock: true,
+}
+
+console.log(product);
+console.log(typeof product);
