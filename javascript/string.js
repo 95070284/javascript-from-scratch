571f63cb20a2690a81c5d3078eadda9b7de382e4
Md Abu Talib <talisman702k@gmail.com>
2026-02-15 01:18:17 +0530

    string in js

---
+++ javascript/string.js
@@ -0,0 +1,15 @@
+// string in js
+
+let str=" Mohd Abutalib"
+
+let name ="Naman"
+console.log(str)
+
+console.log(str.slice(0,5))
+console.log(str.concat(name))
+
+let newName = name.replace("m","y")
+
+console.log(newName)
+
+console.log(str.charAt(5))
