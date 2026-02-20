a5ea550f3f542cf524c15c1a69e87a37de436f48
Md Abu Talib <talisman702k@gmail.com>
2026-02-20 23:37:12 +0530

    somthing

---
+++ javascript/method.js
@@ -0,0 +1,19 @@
+let name = "Mohd AbuTalib "
+
+const countVowels = function (str) {
+    let count = 0;
+    for(let i of str){
+        if (i == "a" || i == "e" || i == "i" || i == "o" || i == "u") {
+            count++
+            console.log(i)
+        }
+
+    } console.log(count)
+}
+
+countVowels(name)  
+let a;
+let A;
+if(A===a)
+    console.log("true")
+else console.log("false")
